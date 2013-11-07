3e763967e73fbfe75b85d7358881eb3bbab53de3
zager00 <estherleeyuling@gmail.com>
2013-11-07 14:58:06 -0500

    small edit

--- src/com/example/chfmeal_tracker/HistoryLogActivity.java
+++ src/com/example/chfmeal_tracker/HistoryLogActivity.java
@@ -1,15 +1,35 @@
 package com.example.chfmeal_tracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class HistoryLogActivity extends Activity {
-
+	DatabaseHandler dh;
+	ListView lv;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_history_log);
+		setContentView(R.layout.history_log);
+		
+		dh = DatabaseHandler.getInstance();
+		final List<Meal> mealList = new ArrayList<Meal>();
+		
+		lv.setAdapter(new MatchListAdapter(this,mealList));
+		
+		mealList = dh.getMeals();
 		
 	}
 
@@ -19,5 +39,26 @@ public class HistoryLogActivity extends Activity {
 		getMenuInflater().inflate(R.menu.history_log, menu);
 		return true;
 	}
+	
+	//reference is SelectMealTypeActivity
+	
+	//==============================================================
+
+	
+	
+	
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
 
 }
