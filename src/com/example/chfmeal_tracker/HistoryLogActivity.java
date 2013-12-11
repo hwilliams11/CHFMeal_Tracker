2e32a3bca20a9823f11dbc473d3febb55c82d236
zager00 <estherleeyuling@gmail.com>
2013-12-10 20:58:56 -0500

    list to tab

--- src/com/example/chfmeal_tracker/HistoryLogActivity.java
+++ src/com/example/chfmeal_tracker/HistoryLogActivity.java
@@ -14,7 +14,42 @@ import android.widget.ExpandableListView.OnGroupClickListener;
 import android.widget.ExpandableListView.OnGroupCollapseListener;
 import android.widget.ExpandableListView.OnGroupExpandListener;
 import android.widget.Toast;
+
+
+import android.view.Menu;
+import android.widget.ListView;
+
  
+public class HistoryLogActivity extends Activity {
+
+	DatabaseHandler dh;
+	ListView lv;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_history_log);
+	
+		dh = DatabaseHandler.getInstance();
+		lv = (ListView)findViewById(R.id.historyListView);
+		final ArrayList<Meal> history = new ArrayList<Meal>();
+		
+		HistoryListAdapter adapter = new HistoryListAdapter(this,history,dh);
+		lv.setAdapter(adapter);
+		dh.getMeals(history);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.history_log, menu);
+		return true;
+	}
+
+}
+
+
+
+/*
 public class HistoryLogActivity extends Activity {
  
     ExpandableListAdapter listAdapter;
@@ -40,9 +75,9 @@ public class HistoryLogActivity extends Activity {
         expListView.setAdapter(listAdapter);
     }
  
-    /*
-     * Preparing the list data
-     */
+    
+     // Preparing the list data
+     
     
     //get data here
     
@@ -104,6 +139,8 @@ public class HistoryLogActivity extends Activity {
 		
     }
 }
+
+*/
 /*
  * 
  *
